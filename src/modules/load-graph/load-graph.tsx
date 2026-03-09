983693d84d0b0641f8faee92db3112c5a5fc9311
Amal221200 <amalmurikkoli2000@gmail.com>
2026-03-09 19:47:25 +0530

    fix: removed hardcoded date

--- src/modules/load-graph/load-graph.tsx
+++ src/modules/load-graph/load-graph.tsx
@@ -12,7 +12,7 @@ import { convertToUTCHoursFormat } from "@/common/utils/helpers";
 import ExportScheduleButton from "../consumer/components/ExportScheduleButton";
 import PageLoader from "@/common/components-ui/page-loader/page-loader";
 
-const presentDate = dayjs("2026-03-10");
+const presentDate = dayjs();
 
 const CHART_COLORS = ['#3b82f6', '#10b981', '#f59e0b', '#ef4444', '#8b5cf6', '#ec4899'];
 
